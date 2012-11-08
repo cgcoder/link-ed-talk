6908b655f538e85cf298b59488c3437de71df908
Chandrasekaran <gopinath.chand@gmail.com>
2012-11-08 02:01:26 -0800

    first iteration dialog manager

--- public/javascripts/dialogManager2.js
+++ public/javascripts/dialogManager2.js
@@ -5,17 +5,63 @@
 /// all the events that this dialogManager want's outside world to know, will
 /// use the notify using the 'listener'
 ///
-function DialogManager(name, listener) {
+function DialogManager(divName, listener) {
 	this.listener = listener;
+	this.divName = divName;
 	
+	this.divId = '#' + this.divName;
+		
 	/// return the chat dialog of the given user
 	this.findDialogForUser = function(userId /*str*/) {
-		
+		return 'dialog_' + userId;
 	};
 	
 	/// create dialog for the given user
 	this.createDialogForUser = function(userId /*str*/) {
+		divObj = $(this.divId);
 		
+		newDivName = this.divName + '_' + userId;
+		newDivId = '#' + newDivName;
+		
+		$('<div></div>', {
+			id: newDivName,
+			"class": "draggable-window"
+		}).appendTo(divObj);
+		
+		$('<div></div>', {
+			"class": "dialog-title",
+			id: newDivName + "_1"
+		}).appendTo($(newDivId));
+		
+		$('<div></div>', {
+			"class": "right",
+			"style": "padding-right: 2px;"
+		}).appendTo($(newDivId + "_1"));
+		
+		$('<div></div>', {
+			"class": "dialog-body"
+		}).appendTo($(newDivId));
+		
+		$('<textarea rows="2" cols="42" style="font-size: 12px;"></textarea>').
+			appendTo($(newDivId));
+		
+		$('<div></div>', {
+			"class": "dialog-status"
+		}).appendTo(newDivId);
+		
+		$(newDivId).draggable();
+		
+		$(newDivId + "_1").hover(function() {
+			$(newDivId).draggable({ disabled: false });
+		}, function() {
+			$(newDivId).draggable({ disabled: true });
+		});
+		
+		return newDivId;
+	};
+	
+	this.setTitleForDialog = function(dialog, title) {
+		$(dialog + "_1").append(title);
 	};
 	
 	/// destroy dialog (close)
